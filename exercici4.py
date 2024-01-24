76e9bd4bc16f92e6440aa000ad2357d7a2cfb803
= <=>
2024-01-24 18:53:12 +0100

    Ejercicio 4

---
+++ exercici4.py
@@ -0,0 +1,6 @@
+# Demanar a l’usuari dos valors. Una vegada s’executa el programa, aquest ha de mostrar el resultat per pantalla.
+
+num1 = input("Numero 1: ")
+num2 = input("Numero 2: ")
+
+print(int(num1)+int(num2))
