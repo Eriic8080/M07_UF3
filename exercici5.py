297d60a9a0fea25d0337cc239c839f283045dc2b
= <=>
2024-01-24 18:57:26 +0100

    Ejercicio 5

---
+++ exercici5.py
@@ -0,0 +1,8 @@
+# Demanar a l’usuari un número. Indicar si el número inserit per l’usuari és parell o senar.
+
+numero = input("Introduce un numero: ")
+
+if int(numero) % 2 ==0:
+    print("Es parell")
+else:
+    print("Es Senar")
