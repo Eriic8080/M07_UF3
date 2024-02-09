0b90b54c04af197d3e356ac39eb19c39a37365ec
= <=>
2024-02-09 16:32:34 +0100

    En processo de la lectura

--- practica2/read.py
+++ practica2/read.py
@@ -4,10 +4,13 @@ def leer(connection,conn):
 
         sql = '''
                 Select * from SERIES
-        )'''
+            '''
 
 
         connection.execute(sql)
-
         conn.commit()
+
+        series = conn.feachall()
         print("Lectura Hecha")
+        print(series)
+        
