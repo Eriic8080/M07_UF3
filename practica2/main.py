6635d5cc1e81ba8f1b6267fc1c11e5c080cfad13
= <=>
2024-02-09 16:57:35 +0100

    MAIN

--- practica2/main.py
+++ practica2/main.py
@@ -2,20 +2,50 @@ import psycopg2
 
 from conn import conectar
 from create_table import tabla
+from create import crearDatos
 from read import leer
+from update import modificar
+from delete import borrarDatos
 
 try:
     #Connexion a la BD
     print("Conexion:")
     connection, conn = conectar()
+    print()
     
     #Funcion para crear una tabla
     print("Creació tabla:")
     tabla(connection, conn)
+    print()
+
+
+    #Funcion Introducir datos a la tabla
+    print("Insertar Datos:")
+    crearDatos(connection, conn)
+    print()
+
 
     #Funcion leer tabla
-    print("Lectura de la tabla")
+    print("Lectura de la tabla:")
     leer(connection, conn)
+    print()
+
+
+    #Funcion para modificar Datos de la tabla
+    modificar(connection,conn)
+    print()
+
+    #Printamos otra vez los datos para ver los cambios
+    leer(connection, conn)
+    print()
+
+
+    #Funcion para ELIMINAR Datos de la tabla
+    borrarDatos(connection, conn)
+    print
+
+
+
 
 except (Exception, psycopg2.Error) as error:
 
