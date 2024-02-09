c231e89d37a692546c2503256bafcf03b86aafb7
= <=>
2024-02-09 17:23:32 +0100

    MAIN hecho solo faltan las capturas

--- practica2/main.py
+++ practica2/main.py
@@ -16,36 +16,51 @@ try:
     #Funcion para crear una tabla
     print("Creació tabla:")
     tabla(connection, conn)
+    crear = input("Continuar?: ")
     print()
 
+    if(crear == "si"):
 
-    #Funcion Introducir datos a la tabla
-    print("Insertar Datos:")
-    crearDatos(connection, conn)
-    print()
+        #Funcion Introducir datos a la tabla
+        print("Insertar Datos:")
+        crearDatos(connection, conn)
+        insertar = input("Continuar?: ")
+        print()
 
+        if(insertar == 'si'):
 
-    #Funcion leer tabla
-    print("Lectura de la tabla:")
-    leer(connection, conn)
-    print()
+            #Funcion leer tabla
+            print("Lectura de la tabla:")
+            leer(connection, conn)
+            lectura = input("Continuar?: ")
+            print()
 
+            if(lectura == "si"):
 
-    #Funcion para modificar Datos de la tabla
-    modificar(connection,conn)
-    print()
+                #Funcion para modificar Datos de la tabla
+                modificar(connection,conn)
+                print()
+                #Printamos otra vez los datos para ver los cambios
+                leer(connection, conn)
+                borrar = input("Continuar?: ")
+                print()
 
-    #Printamos otra vez los datos para ver los cambios
-    leer(connection, conn)
-    print()
+                if(borrar == "si"):
 
+                    #Funcion para ELIMINAR Datos de la tabla
+                    borrarDatos(connection, conn)
+                    print()
 
-    #Funcion para ELIMINAR Datos de la tabla
-    borrarDatos(connection, conn)
-    print
+                else:
+                    print("FIN")
+            else:
+                print("FIN")
+        else:
+            print("FIN")
 
-
-
+    else:
+        print("FIN")
+    
 
 except (Exception, psycopg2.Error) as error:
 
